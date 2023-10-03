e66f89654fc8dab768d7f8b165ac88a210b38522
kaiyu li <krisfk@gmail.com>
2023-10-04 01:20:02 +0800

    .

--- wp-content/themes/starseed/header.php
+++ wp-content/themes/starseed/header.php
@@ -76,7 +76,7 @@
                                 <div class="row align-items-center top-menu-div">
                                     <div class="col-2">
                                         <a href="<?php echo get_site_url();?>" class="logo-a">
-                                            <img src="<?php echo get_template_directory_uri();?>/assets/images/desktop-logo.png"
+                                            <img src="https://galacticstarseedacademy.com/wp-content/themes/starseed/assets/images/desktop-logo.png"
                                                 alt="">
                                         </a>
 
