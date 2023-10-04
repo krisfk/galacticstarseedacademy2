56394b0aa09277e1afe87f86618d71c591df2c1c
kaiyu li <krisfk@gmail.com>
2023-10-04 17:54:53 +0800

    .

--- wp-content/themes/starseed/page-home.php
+++ wp-content/themes/starseed/page-home.php
@@ -35,7 +35,7 @@ get_header(); ?>
 
 
 
-<div class="row align-items-center mt-0 mobile-column-reverse w-bg">
+<div class="row align-items-center mt-0 mobile-column-reverse w-bg  pt-5 pb-5">
 
     <div class="col-lg-12 col-md-12 col-sm-12 col-12 txt-top content-txt ">
 
@@ -77,7 +77,7 @@ get_header(); ?>
 
 </div>
 
-<div class="row g-bg ">
+<div class="row g-bg  pt-5 pb-5">
 
     <div class="col-12 position-relative">
 
