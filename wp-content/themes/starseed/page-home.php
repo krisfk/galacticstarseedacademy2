c76ac27cbe464afba0d12112d5922bf61323fbcc
kaiyu li <krisfk@gmail.com>
2023-10-12 03:04:47 +0800

    .

--- wp-content/themes/starseed/page-home.php
+++ wp-content/themes/starseed/page-home.php
@@ -19,84 +19,10 @@ get_header(); ?>
 
 
 
-<!-- <img class="star0 fadein-ele" style=" width: 500px !important;position: absolute;top: 82px;left: -444px;"
-    src="<?php echo get_template_directory_uri();?>/assets/images/star0.png" alt="">
-
-<img class="star2 fadein-ele" style="  width: 320px !important;position: absolute;top: -2px;left: 540px;"
-    src="<?php echo get_template_directory_uri();?>/assets/images/star2.png" alt="">
-
-<img class="star3 fadein-ele " style=" width: 300px !important;position: absolute;top: 120px;right: -163px;"
-    src="<?php echo get_template_directory_uri();?>/assets/images/star3.png" alt="">
-
-<img class="star4 fadein-ele" style="width: 170px !important;position: absolute;bottom: 51px;left: 600px;"
-    src="<?php echo get_template_directory_uri();?>/assets/images/star4.png" alt=""> -->
-
-
-
 </div>
 
 
 
-
-
-
-<div class="row align-items-center mt-0 mobile-column-reverse">
-
-    <div class="col-lg-12 col-md-12 col-sm-12 col-12 txt-top content-txt  w-bg  pt-4 pb-5">
-
-
-        <div class="text-center"><img
-                src="https://galacticstarseedacademy.com/wp-content/uploads/2023/03/desktop-logo-300x300.png" alt="">
-        </div>
-
-        <?php //echo get_field('content_1');?>
-
-        <div class="text-center">
-
-
-            <h1>正在尋找高階的靈性課程？</h1>
-            <div class="content-txt-div mt-4">
-                星際綜合能量療法 <br>
-                一個絕對不會讓你後悔的靈性課程
-
-
-            </div>
-
-            <a href="<?php echo get_site_url();?>/key" class="know-more-btn">立即了解更多</a>
-
-
-        </div>
-
-
-
-        <!-- 正在尋找高階的靈性課程？
-星際綜合能量療法
-一個絕對不會讓你後悔的靈性課程
-立即了解更多 -->
-
-
-    </div>
-</div>
-
-<!-- <div class="row pt-5 pb-5 g-bg">
-
-    <div class="container text-center">
-        <h1>成為會員</h1>
-
-        <div class="content-txt-div mt-4">
-            用最低成本，學習最高階的靈性技術
-
-
-
-        </div>
-        <a href="<?php echo get_site_url();?>/ascension-temple-login" class="know-more-btn">立即了解更多</a>
-
-
-    </div>
-
-
-
-</div> -->
 <div class="row pt-5 pb-5 g-bg">
 
     <div class="container  position-relative">
@@ -174,43 +100,7 @@ get_header(); ?>
 </div>
 
 
-<!-- 
-<div class="bottom-container-wrapper">
-    <div class="container">
 
-        <div class="row align-items-center position-relative h-100">
-
-
-            <div class="col-lg-6 col-md-12 col-sm-12 col-12  position-relative key-bg">
-
-                <div class="content-txt me-5 px-2">
-                    <?php echo get_field('content_4');?>
-
-                    <a href="<?php echo get_site_url();?>/key" class="know-more-btn">了解更多</a>
-
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-12 col-sm-12 col-12 position-relative bud-bg ">
-
-
-                <div class="content-txt me-5 px-2">
-
-                    <?php echo get_field('content_5');?>
-
-
-                    <a href="<?php echo get_site_url()?>/product/多次元靈魂療癒/" class="know-more-btn">了解更多</a>
-
-
-                </div>
-
-
-            </div>
-
-
-        </div>
-    </div>
-</div>
- -->
 
 <?php
 
