d82ad02595be97813a5cb57f75d0c60509f17acf
kaiyu li <krisfk@gmail.com>
2023-10-18 00:37:36 +0900

    .

--- wp-content/themes/starseed/header.php
+++ wp-content/themes/starseed/header.php
@@ -18,6 +18,8 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#319197">
+
     <?php wp_head(); ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
