f68fbc33a2bbc3c541533c226732dbcaa7d181c9
kaiyu li <krisfk@gmail.com>
2023-10-21 13:17:04 +0900

    .

--- wp-content/themes/starseed/header.php
+++ wp-content/themes/starseed/header.php
@@ -289,6 +289,10 @@ if( pms_is_member_of_plan( array( 178 ) )  || $expired)
                                     $('html').css({
                                         'overflow': 'hidden'
                                     });
+                                    // 0px 0px 10px 0px rgb(0 0 0 / 50%)
+                                    $('.top-menu-container').css({
+                                        'box-shadow': 'none'
+                                    });
 
                                     // $('.top-menu-container').css({
                                     //     'height': '100%'
@@ -299,6 +303,13 @@ if( pms_is_member_of_plan( array( 178 ) )  || $expired)
 
 
                                 } else {
+
+                                    // 0px 0px 10px 0px rgb(0 0 0 / 50%)
+                                    $('.top-menu-container').css({
+                                        'box-shadow': '0px 0px 10px 0px rgb(0 0 0 / 50%)'
+                                    });
+
+
                                     $('.top-menu-ul').fadeOut(0);
                                     $('.outer-menu-div').css({
                                         'height': '77px'
