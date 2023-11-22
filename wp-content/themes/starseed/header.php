901d70e23a672f3061819407d9eccb053fe44dfb
kaiyu li <krisfk@gmail.com>
2023-11-23 02:28:46 +0900

    .

--- wp-content/themes/starseed/header.php
+++ wp-content/themes/starseed/header.php
@@ -357,12 +357,16 @@ if( pms_is_member_of_plan( array( 178 ) )  || $expired)
 
 
 
+
+                                    });
+
+                                    setTimeout(() => {
+
                                         $('.top-menu-container').css({
                                             'box-shadow': '0px 0px 10px 0px rgb(0 0 0 / 50%)'
                                         });
 
-
-                                    });
+                                    }, 200);
 
 
                                     $('.outer-menu-div').css({
